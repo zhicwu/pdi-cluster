27c83cf508085e8e496497ed27a90c3076f78c8d
Zhichun Wu <zhicwu@gmail.com>
2016-12-05 12:19:36 +0800

    Set timeout or it may stuck there forever

--- pentaho-kettle/src/main/java/org/pentaho/di/trans/steps/mail/Mail.java
+++ pentaho-kettle/src/main/java/org/pentaho/di/trans/steps/mail/Mail.java
@@ -69,6 +69,11 @@ import java.util.zip.ZipOutputStream;
 public class Mail extends BaseStep implements StepInterface {
     private static Class<?> PKG = MailMeta.class; // for i18n purposes, needed by Translator2!!
 
+    private final static String KETTLE_MAIL_CONNECTION_TIMEOUT
+            = System.getProperty("KETTLE_MAIL_CONNECTION_TIMEOUT", "10000"); // 10 seconds
+    private final static String KETTLE_MAIL_READ_TIMEOUT
+            = System.getProperty("KETTLE_MAIL_READ_TIMEOUT", "30000"); // 30 seconds
+
     private MailMeta meta;
     private MailData data;
 
@@ -521,6 +526,7 @@ public class Mail extends BaseStep implements StepInterface {
         // create some properties and get the default Session
 
         String protocol = "smtp";
+
         if (meta.isUsingSecureAuthentication()) { // PDI-2955
             // if (meta.isUsingAuthentication()) {
             if (meta.getSecureConnectionType().equals("TLS")) {
@@ -534,6 +540,12 @@ public class Mail extends BaseStep implements StepInterface {
                 data.props.put("mail.smtps.quitwait", "false");
             }
         }
+
+        // set timeout according to:
+        // https://javamail.java.net/nonav/docs/api/com/sun/mail/smtp/package-summary.html
+        data.props.put("mail." + protocol + ".connectiontimeout", KETTLE_MAIL_CONNECTION_TIMEOUT);
+        data.props.put("mail." + protocol + ".timeout", KETTLE_MAIL_READ_TIMEOUT);
+
         data.props.put("mail." + protocol + ".host", server);
         if (port != -1) {
             data.props.put("mail." + protocol + ".port", "" + port); // needs to be supplied as a string, not as an integer
